33116a015366a1017ff215ffdf49a0ed650b2ca9
Jason Nardozza <jnardozza980@gmail.com>
2015-11-08 22:45:30 -0500

    updated references

--- Calendar/submitSchedule.php
+++ Calendar/submitSchedule.php
@@ -1,7 +1,7 @@
 <?php
 //gets a JSON array of schedules for a specific start and end date passed via GET
-require_once "../../../api/InclusionFirst/connect_inclusion_first.php"; //connection for inclusion first
-require_once "../../../api/InclusionFirst/Schedules.php"; //schedule table api
+require_once "../../../inclusionfirst_api/connect_inclusion_first.php"; //connection for inclusion first
+require_once "../../../inclusionfirst_api/Schedules.php"; //schedule table api
 
 $schedules = new Schedules;
 $resources = $schedules->getCalendarResources($inclusionFirst, "Client");
