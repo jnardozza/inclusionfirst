337877387704b18daf05c52b969eca5c65d25c4d
Jason Nardozza <jnardozza980@gmail.com>
2015-11-19 09:10:46 -0500

    enabled POST parameters
    events script will now accept start and end date and resource type

--- Calendar/get_events.php
+++ Calendar/get_events.php
@@ -2,12 +2,19 @@
 require_once "../../../inclusionfirst_api/connect_inclusion_first.php";
 require_once "../../../inclusionfirst_api/Events.php";
 
-#$start = $_GET["start"];
-#$end = $_GET["end"];
+$start_param = $_POST["start"];
+$end_param = $_POST["end"];
+$group = $_POST["group"];
+$timezone = 1;
+
+$start_unix = strtotime($start_param) + 3600 * $timezone+date("I");
+$end_unix = strtotime($end_param) + 3600 * $timezone+date("I");
+
+$start = gmdate('m/d/Y', $start_unix);
+$end = gmdate('m/d/Y', $end_unix);
 
 $schedules = new Events;
-//$scheduleObject = array('Date_Start' => $start, 'Date_End' => $end );
-$scheduleObject = null;
+$scheduleObject = array('date' => ">=".$start, 'dateSearch' => "<=".$end );
 $events = $schedules->getCalendarEvents($scheduleObject, $inclusionFirst, "Client");
 
 echo $events;
